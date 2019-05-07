1abb01d28a4464319f4693bb83248a65a1843c12
Stacy Williams <49852311+StacyWilliams@users.noreply.github.com>
2019-05-07 15:52:31 -0500

    callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,6 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test your work. 
+// You have been provided an example of a problem and a solution to see how this works with our items array.  
+// Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -23,28 +25,77 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   });
 
 */
-
-
-function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+function firstItem(array, callback) {
+  callback(array[0]);
 }
 
-function last(arr, cb) {
+firstItem(items, (first) =>
+ console.log(first) 
+) 
+
+ // getLength passes the length of the array into the callback.
+
+function getLength(array, callback) {
+  callback(array.length);
+}
+
+getLength(items, (length) => 
+console.log(length)
+ 
+)
+
+//function last(arr, cb) {
   // last passes the last item of the array into the callback.
-}
 
-function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+  
+  function lastItem(array, callback) {
+    callback(array[items.length - 1]);
+  }
+  
+ lastItem(items, function (last) {
+   
+   console.log(last) 
+  
+ })
+ 
+// sumNums adds two numbers (x, y) and passes the result to the callback.
+function sumNums(x, y, callback) {
+   return callback(x + y);
+   
+   }
 
-function multiplyNums(x, y, cb) {
+sumNums(7,4, function(addition){
+  console.log(addition)
+});
+  
+  
+
+  
+  
+
+
+
   // multiplyNums multiplies two numbers and passes the result to the callback.
-}
+  function multNums(x, y, callback) {
+    return callback(x * y);
+    
+    }
+ 
+ multNums(7,4, function(multiply){
+   console.log(multiply)
+ });
 
-function contains(item, list, cb) {
+
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  function contains(item, list, cb) {
+    cb(list.includes(item))
+
 }
+     contains('Gum', items, function(result) {
+     console.log(result)
+} )
+
 
 /* STRETCH PROBLEM */
 
